ecea1235efeea9010b55a583cae62a9c5eafefff
adarsh205 <94178153+adarsh205@users.noreply.github.com>
2023-09-24 15:39:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ ip = st.text_input('enter the text')
 op = model.predict([ip])
 if st.button('Predict'):  #a button will be created named 'Predict'
   st.title(op[0])    #output will be displayed as a title
+
